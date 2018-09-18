be33ed66f3bf53a35f164eb507f36e70d8e92c08
ocularrhythm <aaron.g.arney@gmail.com>
2018-09-18 16:30:24 -0400

    Remove duplicated css imports

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,22 +4,15 @@ import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
+import '../global/index.css';
 
 const Layout = ({ children }) => (
     <StaticQuery
-        query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+        query={ graphql`query SiteTitleQuery { site { siteMetadata { title } } }` }
         render={data => (
             <>
                 <Helmet
-                    title={data.site.siteMetadata.title}
+                    title={ data.site.siteMetadata.title }
                     meta={[
                         { name: 'description', content: 'Sample' },
                         { name: 'keywords', content: 'sample, something' },
@@ -28,14 +21,8 @@ const Layout = ({ children }) => (
                     <html lang="en" />
                 </Helmet>
                 <Header />
-                <div
-                    style={{
-                        margin: '0 auto',
-                        maxWidth: 960,
-                        padding: '0px 1.0875rem 1.45rem',
-                        paddingTop: 0,
-                    }}>
-                    {children}
+                <div className="grid">
+                    { children }
                 </div>
             </>
         )}
