060eb549c23a887bc61c1fd1b3700819129c1bb6
Aaron Arney <aaron.g.arney@gmail.com>
2018-09-19 09:01:02 -0400

    Incorporate Archive component into render

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import Helmet from 'react-helmet';
 
+import Archive from '../components/archive';
 import Layout from '../components/layout'
 
-export default () => {
+export default ({ data }) => {
+    const { allMarkdownRemark: edges } = data;
+
     return (
         <Layout>
             <div style={{ marginTop: '75px' }}>
                 <Helmet title="Ocular-Rhythm" bodyAttributes={{
                     class: 'archive'
                 }} />
+                <Archive posts={ edges.edges } />
             </div>
         </Layout>
     );
 };
 
+export const pageQuery = graphql`
+    query IndexQuery {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+            edges {
+                node {
+                    frontmatter {
+                    title
+                    date(formatString: "MMMM DD, YYYY")
+                    path
+                    category
+                }
+            }
+        }
+    }
+}`;
