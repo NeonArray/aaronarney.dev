226f0031ae5bb3552dade1c8b9f9b8af2f52c386
Aaron Arney <aaron.g.arney@gmail.com>
2018-09-19 09:01:33 -0400

    Remove postcss-autoreset
    The autoreset was being prioritized over my css which was causing styles to be ignored

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = () => ({
         require('postcss-custom-media'),
         require('postcss-preset-env')({ stage: 0 }),
         require('postcss-nested'),
-        require('postcss-autoreset'),
+        // require('postcss-autoreset'),
         require('postcss-custom-selectors'),
     ],
 });
