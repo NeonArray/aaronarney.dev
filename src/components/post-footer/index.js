09cdec640d1ab8f136cd550c4bbd6f0984186848
Aaron Arney <aarney@leapsparkagency.com>
2018-09-21 17:46:42 -0400

    Add PostFooter component

---
+++ src/components/post-footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import style from  './post-footer.css';
+
+const PostFooter = ({ links }) => {
+    const { next, prev } = links;
+
+    return (
+        <div className={ style.nav }>
+            <Link to={ prev !== null ? prev.frontmatter.path : '' } className={ style.previous }>
+                <div className={ style.copy }>
+                    <span className={ style.direction }>
+                        Previous Post
+                    </span>
+                    { prev !== null ? prev.frontmatter.title : 'No More Posts' }
+                </div>
+            </Link>
+
+            <Link to={ next !== null ? next.frontmatter.path : '' } className={ style.next }>
+                <div className={ style.copy }>
+                    <span className={ style.direction }>
+                        Next Post
+                    </span>
+                    { next !== null ? next.frontmatter.title : 'No More Posts' }
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default PostFooter;
