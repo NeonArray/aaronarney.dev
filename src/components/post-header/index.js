00ba8b4bc48093d94bdcb52bea79f0b6e6b2745e
Aaron Arney <aarney@leapsparkagency.com>
2018-09-21 17:46:50 -0400

    Add PostHeader componetn

---
+++ src/components/post-header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import style from './post-header.module.css';
+
+import vr from '../../images/vr.svg';
+import code from '../../images/code.svg';
+import general from '../../images/general.svg';
+import software from '../../images/software.svg';
+import web from '../../images/web.svg';
+
+const img = {
+    vr, code, general, software, web,
+};
+
+const PostHeader = ({ content }) => {
+    const category = 'post-category color__' + content.frontmatter.category;
+
+    return (
+        <header className={ style.header }>
+            <div className={ style.left }>
+                <img alt="Man wearing a virtual reality headset" src={ img[content.frontmatter.category.toLowerCase()] } height="300" width="300" className={ style.image } />
+            </div>
+            <div className={ style.right }>
+                <div className={ category }>
+                    { content.frontmatter.category }
+                </div>
+                <h1 className="post-title">
+                    <span className="post-title--link">
+                        { content.frontmatter.title }
+                    </span>
+                </h1>
+                <div className="post-date">
+                    { content.frontmatter.date }
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default PostHeader;
