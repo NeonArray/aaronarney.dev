be6cd846ca0ff1f68d48db20f69b040a68c36d4b
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-09-22 14:32:41 -0400

    Render PostBody and PostFooter components

--- src/templates/single.js
+++ src/templates/single.js
@@ -1,33 +1,64 @@
-import React from "react";
-import Helmet from "react-helmet";
+import React from 'react';
+import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
+import moment from 'moment';
 
 import Layout from '../components/layout';
+import Callout from '../components/callout';
+import PostHeader from '../components/post-header';
+import PostBody from '../components/post-body';
+import PostFooter from '../components/post-footer';
 
+function shouldShowCallout(date) {
+    const currentDate = moment();
+    const postDate = moment(date);
 
+    if (currentDate.diff(postDate, 'years') >= 1) {
+        return (<Callout message={`This article was published over ${currentDate.diff(postDate, 'years') } ago.`}/>);
+    }
 
-export default ({ data, pageContext }) => {
+    return undefined;
+}
+
+export default function Template({ data, pathContext }) {
     const post = data.markdownRemark;
+    const { html, frontmatter } = post;
+    const callout = shouldShowCallout(frontmatter.date);
 
     return (
-        <Layout>
-            <article className="post">
-                <Helmet title={`Your Blog Name - ${ post.frontmatter.title }`} bodyAttributes={{
-                    class: 'page-post'
-                }} />
-            </article>
+        <Layout location={data.location}>
+            <Helmet title={ frontmatter.title } bodyAttributes={{
+                class: 'page-post'
+            }} />
+
+            <main className="main" data-grid-area="main">
+                <article className="post">
+
+                    <PostHeader content={ frontmatter } />
+
+                    { callout }
+
+                    <PostBody content={ post } />
+
+                    <PostFooter links={ pathContext } />
+                </article>
+            </main>
         </Layout>
     );
 }
 
 export const pageQuery = graphql`
-    query($path: String!) {
+    query BlogPostBySlug($path: String!) {
+        site {
+            siteMetadata {
+                title
+            }
+        }
         markdownRemark(frontmatter: { path: { eq: $path } }) {
             html
             frontmatter {
-                date(formatString: "MMMM DD, YYYY")
-                path
                 title
+                date(formatString: "MMMM DD, YYYY")
                 category
             }
         }
