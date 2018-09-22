7b3d30310e32e05e02f148e5f3d3b3a8adf2e023
Aaron Arney <aaron.arney@ocular-rhythm.com>
2018-09-22 14:32:01 -0400

    Update node config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,26 @@
-const path = require('path');
+const path = require("path");
 const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
+exports.onCreateNode = ({ node, actions, getNode }) => {
     const { createNodeField } = actions;
 
     if (node.internal.type === `MarkdownRemark`) {
-        const slug = createFilePath({ node, getNode, basePath: `pages` });
+        const value = createFilePath({ node, getNode });
         createNodeField({
-            node,
             name: `slug`,
-            value: slug,
+            node,
+            value,
         });
     }
 };
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions;
 
-    const blogPostTemplate = path.resolve('src/templates/single.js');
-    const blogCategoryTemplate = path.resolve('src/templates/category.js');
+    const blogPostTemplate = path.resolve(`src/templates/single.js`);
 
     return new Promise((resolve, reject) => {
-        graphql(
-            `{
-            allMarkdownRemark(
+        resolve(graphql(`{ allMarkdownRemark(
                 sort: { order: DESC, fields: [frontmatter___date] }
                 limit: 1000
             ) {
@@ -31,56 +28,28 @@ exports.createPages = ({ graphql, actions }) => {
                     node {
                         frontmatter {
                             path
-                            title
-                            category
                         }
                     }
                 }
-            }
-        }`).then(result => {
-            if (result.errors) {
-                return Promise.reject(result.errors);
-            }
+            } }`).then(result => {
+                if (result.errors) {
+                    reject(result.errors);
+                }
 
-            const posts = result.data.allMarkdownRemark.edges;
+                const posts = result.data.allMarkdownRemark.edges;
 
-            // Using a leading "/" will generate a different page than one without. I'm just going to ensure these
-            // always format with a leading slash since that seems to be what Gatsby is expecting.
-            const pathRegex = new RegExp('/(^\/)/', 'g');
-
-            result.data.allMarkdownRemark.edges
-                .forEach(({ node }, index) => {
-                    const normalizedPath = pathRegex.test(node.frontmatter.path) ? node.frontmatter.path : `/${node.frontmatter.path}`;
-
+                result.data.allMarkdownRemark.edges.forEach(({ node }, index) => {
                     createPage({
-                        path: normalizedPath,
+                        path: node.frontmatter.path,
                         component: blogPostTemplate,
                         context: {
                             prev: index === 0 ? null : posts[index - 1].node,
                             next: index === (posts.length - 1) ? null : posts[index + 1].node,
-                        }
+                        },
                     });
                 });
-
-            let categories = [];
-            posts.forEach((edge) => {
-                categories.push(edge.node.frontmatter.category);
-            });
-
-            // Create unique set of categories
-            categories = [...new Set(categories)];
-
-            categories.forEach((category) => {
-                createPage({
-                    path: `blog/category/${category}`,
-                    component: blogCategoryTemplate,
-                    context: {
-                        category,
-                    },
-                });
-            });
-
-            resolve();
-        });
+            })
+        );
     });
 };
+
