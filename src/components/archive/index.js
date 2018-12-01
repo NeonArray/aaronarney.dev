2140d63e14925546c7477113bca0c7d72e390ff3
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-01 13:55:15 -0500

    Formatting

--- src/components/archive/index.js
+++ src/components/archive/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-
 import PostBlock from '../post-block';
-
 import style from './archive.module.css';
 
-const Archive = ({ posts }) => {
+const Archive = ({ posts, layout }) => {
     return (
         <div className={ style.posts }>
             { posts.map((i, c) => {
                 return (
-                    <PostBlock key={ c } meta={ i.node }/>
+                    <PostBlock key={ c } meta={ i.node } layout={ layout } />
                 );
             })}
         </div>
