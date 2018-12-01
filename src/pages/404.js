c723cdd09a4fe5b3e81d37ae45d8b8f50ac9e667
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-01 13:55:39 -0500

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Helmet from 'react-helmet';
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+import Layout from '../components/layout';
 
-export default NotFoundPage
+export default () => (
+    <Layout>
+        <div style={{ marginTop: '75px' }}>
+            <Helmet title="404 Not Found : Ocular-Rhythm" bodyAttributes={{
+                class: ''
+            }} />
+            <main data-grid-area="main">
+                <h1>NOT FOUNDED</h1>
+                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </main>
+        </div>
+    </Layout>
+);
