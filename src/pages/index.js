19543d2c60ab7013d8a25dc7199d0bc9d5800608
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-01 13:53:47 -0500

    Remove graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,20 @@
 import React from 'react'
 import Helmet from 'react-helmet';
-import { graphql } from 'gatsby';
 
-import Archive from '../components/archive';
 import Layout from '../components/layout';
 
-export default ({ data }) => {
-    const { allMarkdownRemark: edges } = data;
+export default () => (
+    <Layout>
+        <div style={{ marginTop: '75px' }}>
+            <Helmet title="Ocular-Rhythm" bodyAttributes={{
+                class: 'page-post'
+            }} />
+            <main data-grid-area="main" style={{ textAlign: 'center' }}>
+                <h1>This is the personal site of Aaron Arney, a developer from Louisville, KY.</h1>
+                <p>I have a penchant for starting tons of projects and finishing very few of them. In fact, this site is not completely finished either! Fun!</p>
 
-    return (
-        <Layout>
-            <div style={{ marginTop: '75px' }}>
-                <Helmet title="Ocular-Rhythm" bodyAttributes={{
-                    class: 'archive'
-                }} />
-                <Archive posts={ edges.edges } />
-            </div>
-        </Layout>
-    );
-};
-
-export const pageQuery = graphql`
-    query IndexQuery {
-        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-            edges {
-                node {
-                    frontmatter {
-                    title
-                    date(formatString: "MMMM DD, YYYY")
-                    path
-                    category
-                }
-            }
-        }
-    }
-}`;
+                <p>I built this using <a className="link" href="https://www.gatsbyjs.org" target="_blank" rel="noopener nofollow noreferrer">Gatsby 2.0</a>, it's interesting.</p>
+            </main>
+        </div>
+    </Layout>
+);
