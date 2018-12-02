89dc421d4294260409a9342a0eb467cc9f3c51b0
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-02 14:07:18 -0500

    Grammar

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,9 +9,9 @@ export default () => (
             <Helmet title="404 Not Found : Ocular-Rhythm" bodyAttributes={{
                 class: ''
             }} />
-            <main data-grid-area="main">
-                <h1>NOT FOUNDED</h1>
-                <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <main data-grid-area="main" style={{ margin: '0 auto', maxWidth: '920px', textAlign: 'center' }}>
+                <h1>I don't think this is what you we're hoping to find. In technical jargon, this is a 404 error.</h1>
+                <p>You just hit a page that doesn&#39;t exist... the sadness.</p>
             </main>
         </div>
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ export default () => (
             <Helmet title="Ocular-Rhythm" bodyAttributes={{
                 class: 'page-post'
             }} />
-            <main data-grid-area="main" style={{ textAlign: 'center' }}>
-                <h1>This is the personal site of Aaron Arney, a developer from Louisville, KY.</h1>
+            <main data-grid-area="main" style={{ textAlign: 'center', maxWidth: '920px', margin: '0 auto' }}>
+                <h1 style={{ lineHeight: '1.25em', fontSize: '2rem' }}>This is a blog about development by and of Aaron Arney, a developer from Louisville, KY.</h1>
                 <p>I have a penchant for starting tons of projects and finishing very few of them. In fact, this site is not completely finished either! Fun!</p>
 
                 <p>I built this using <a className="link" href="https://www.gatsbyjs.org" target="_blank" rel="noopener nofollow noreferrer">Gatsby 2.0</a>...it's interesting.</p>
