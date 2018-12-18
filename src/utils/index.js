a0a3dd5c6d4b10fc7f4e0aab69b31c8fa8886047
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-18 18:55:29 -0500

    Add capitalizeWords helper function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,7 @@
 export function assign(...classes) {
     return [...classes].join(' ');
 }
+
+export function capitalizeWords(str) {
+    return str.replace(/\b\w/g, letter => letter.toUpperCase());
+}
