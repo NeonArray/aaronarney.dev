a135d794843ec8da07d1330bbe2447f002c0dd00
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-20 12:29:05 -0500

    Modify content security policy header

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
             options: {
                 headers: {
                     '/*': [
-                        'Content-Security-Policy: default-src https://ocular-rhythm.io:*',
+                        'Content-Security-Policy: default-src ocular-rhythm.io',
                         'Referrer-Policy: no-referrer',
                         'Feature-Policy: vibrate "none"; geolocation "none"; midi "none"; notifications "none"; push "none"; sync-xhr "none"; microphone "none"; camera "none"; magnetometer "none"; gyroscope "none"; speaker "none"; fullscreen "none"; payment "none";'
                     ]
