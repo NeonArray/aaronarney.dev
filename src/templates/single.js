af0a1b0abf6c6766fbe43cfcb93e3ba2db0a4dab
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-20 14:56:30 -0500

    Format the date in the graphql query to match moments requirement

--- src/templates/single.js
+++ src/templates/single.js
@@ -58,7 +58,7 @@ export const pageQuery = graphql`
             html
             frontmatter {
                 title
-                date(formatString: "MMMM DD, YYYY")
+                date(formatString: "MMMM-DD-YYYY")
                 category
             }
         }
