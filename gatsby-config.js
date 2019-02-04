220cfdaeaa6e8661d855520c9af580e2086237b6
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-04 10:49:48 -0500

    Remove quotes from CSP

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
             options: {
                 headers: {
                     '/*': [
-                        'Content-Security-Policy: default-src \'self\'; script-src \'self\' \'unsafe-inline\' \'http://ec2-54-173-166-214.compute-1.amazonaws.com\'; connect-src \'self\'; img-src https://*.ocular-rhythm.io data:; style-src \'self\' \'unsafe-inline\';',
+                        'Content-Security-Policy: default-src \'self\'; script-src \'self\' \'unsafe-inline\' http://ec2-54-173-166-214.compute-1.amazonaws.com; connect-src \'self\'; img-src https://*.ocular-rhythm.io data:; style-src \'self\' \'unsafe-inline\';',
                         'Referrer-Policy: no-referrer',
                         'Feature-Policy: vibrate "none"; geolocation "none"; midi "none"; notifications "none"; push "none"; sync-xhr "none"; microphone "none"; camera "none"; magnetometer "none"; gyroscope "none"; speaker "none"; fullscreen "none"; payment "none";'
                     ]
