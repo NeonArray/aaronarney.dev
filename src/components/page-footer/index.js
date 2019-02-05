1b85ce2963a42e888bbe6c204f0dcc25a69f22d6
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-05 11:11:03 -0500

    Add PageFooter component

---
+++ src/components/page-footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import style from './page-footer.module.css';
+
+
+export default function PageFooter() {
+    return (
+        <div data-grid-area="page-footer" className={style.footer}>
+            <Link to="/privacy-policy">
+                Privacy Policy
+            </Link>
+        </div>
+    );
+}
