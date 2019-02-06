8a83cd7fa1a1a9a3e21b56a242c71353725fa23e
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-05 21:55:41 -0500

    Remove VR from dictionary

--- src/components/category-image/index.js
+++ src/components/category-image/index.js
@@ -1,31 +1,31 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import vrImage from '../../images/vr.svg';
-import codeImage from '../../images/code.svg';
+// import vrImage from '../../images/vr.svg';
+// import codeImage from '../../images/code.svg';
 import generalImage from '../../images/general.svg';
 import softwareImage from '../../images/software.svg';
 import webImage from '../../images/web.svg';
 
 const dictionary = {
-    vr: {
-        alt: 'Man wearing a virtual reality headset',
-        src: vrImage,
-    },
-    code: {
-        alt: '',
-        src: codeImage,
-    },
+    // vr: {
+    //     alt: 'Man wearing a virtual reality headset',
+    //     src: vrImage,
+    // },
+    // code: {
+    //     alt: '',
+    //     src: codeImage,
+    // },
     general: {
-        alt: '',
+        alt: 'An owl winking',
         src: generalImage,
     },
     software: {
-        alt: '',
+        alt: 'Abstract computer screen and pie charts',
         src: softwareImage,
     },
     web: {
-        alt: '',
+        alt: 'A text editor with binary code',
         src: webImage,
     },
 };
