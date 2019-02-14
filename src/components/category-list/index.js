274756390d6aaba8d593a9c9f0b08912a3ddcc40
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-14 16:55:50 -0500

    Add tailwind class to component

--- src/components/category-list/index.js
+++ src/components/category-list/index.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import { assign } from '../../utils';
-
-import helpers from '../../global/helpers.module.css';
-import style from './category-list.module.css';
 import CategoryImage from "../category-image";
 
 
 export default function CategoryList() {
     return (
-        <div className={helpers.showForMobile}>
-            <div className={style.blocks}>
-                <p className="section-label">Categories</p>
+        <div className="sm:block md:hidden">
+            <div className="block px-5 md:hidden">
+                <p className="text-center mb-3">Categories</p>
 
-                <ul className={assign(style.categories, helpers.listInline)}>
+                <ul className="list-reset flex flex-row justify-around mb-6 text-center">
                     <li>
-                        <Link to="blog/category/software">
-                            <CategoryImage height={50} width={50} category="software" cssClass={style.image} />
+                        <Link to="blog/category/software" className="no-underline text-xs">
+                            <CategoryImage
+                                height={50}
+                                width={50}
+                                category="software"
+                                cssClass="block h-12 mb-1 w-auto"
+                            />
                             Software
                         </Link>
                     </li>
                     <li>
-                        <Link to="blog/category/web">
-                            <CategoryImage height={50} width={50} category="web" cssClass={style.image} />
+                        <Link to="blog/category/web" className="no-underline text-xs">
+                            <CategoryImage
+                                height={50}
+                                width={50}
+                                category="web"
+                                cssClass="block h-12 mb-1 w-auto"
+                            />
                             Web
                         </Link>
                     </li>
                     <li>
-                        <Link to="blog/category/general">
-                            <CategoryImage height={50} width={50} category="general" cssClass={style.image} />
+                        <Link to="blog/category/general" className="no-underline text-xs">
+                            <CategoryImage
+                                height={50}
+                                width={50}
+                                category="general"
+                                cssClass="block h-12 mb-1 w-auto"
+                            />
                             General
                         </Link>
                     </li>
