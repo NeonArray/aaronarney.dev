947aa064ee61f686ee82e72c750110b5c7095d5a
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-14 16:56:01 -0500

    Add tailwind class to component

--- src/components/post-body/index.js
+++ src/components/post-body/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import style from './post-body.module.css';
 import 'prismjs/themes/prism-okaidia.css';
 
 
@@ -15,7 +14,7 @@ export default function PostBody({ content }) {
      * Alas, this route is not ideal and it also introduces a roadblock that prevents my "Copy" code button from functioning. I may have to extract
      * all of this out into it's own plugin at some point and find a more robust and elegant solution.
      */
-    let html = `<div class="${style.copy}">`;
+    let html = `<div class="text-xl font-light p-3 md:p-0 md:max-w-max post-copy">`;
     // eslint-disable-next-line
     const MATCH_CODE_BLOCKS = new RegExp('<div class="gatsby-highlight" data-language="([a-z]*)?">(\t|\r|\n|.)*?<\/div>', 'g');
 
@@ -29,9 +28,9 @@ export default function PostBody({ content }) {
     const contentContainsMarkup = MATCH_CODE_BLOCKS.test(html);
 
     html += content.html.replace(MATCH_CODE_BLOCKS,
-        `</div><div class="${style.block}"><div class="${style.heading}">` +
-        `<p class="${style.language}">$1</p></div>` +
-        `<div class="${style.wrapper} gatsby-highlight">$&</div></div><div class="${style.copy}">`
+        `</div><div class="align-center bg-black-lighter my-16 mx-0 w-screen"><div class="m-0 -mr-4 max-w-none px-3 py-0 relative bg-grey-lighter">` +
+        `<p class="block font-bold leading-loose mt-0 text-grey-darker uppercase md:text-center">$1</p></div>` +
+        `<div class="mx-auto my-0 max-w-xl gatsby-highlight">$&</div></div><div class="text-xl font-light p-3 md:p-0 md:max-w-max post-copy">`
     );
 
     if (contentContainsMarkup) {
@@ -39,7 +38,7 @@ export default function PostBody({ content }) {
     }
 
     return (
-        <section className={style.post} dangerouslySetInnerHTML={{ __html: html }} itemProp="articleBody">
+        <section className="items-center flex flex-col justify-end mt-0" dangerouslySetInnerHTML={{ __html: html }} itemProp="articleBody">
 
         </section>
     );
