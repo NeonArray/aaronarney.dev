e864a641a8c2da7e1a8a985c9901803da57f5ff7
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-16 10:12:59 -0500

    Replace css with tailwind

--- src/components/post-footer/index.js
+++ src/components/post-footer/index.js
@@ -7,19 +7,19 @@ export default function PostFooter({ links }) {
     const { next, prev } = links;
 
     return (
-        <div data-grid-area="footer" className="flex flex-col font-4 mt-12 relative w-screen md:flex-row">
-            <Link to={prev !== null ? prev.frontmatter.path : ''} className="paginate bg-secondary-grad text-white text-right">
-                <div className="" style={{ textOverflow: 'hidden' }}>
-                    <span className={style.direction}>
+        <div data-grid-area="footer" className="post-nav flex flex-col font-4 mt-12 relative w-screen md:flex-row">
+            <Link to={prev !== null ? prev.frontmatter.path : ''} className="paginate secondary-grad text-white text-right">
+                <div className="truncate relative font-bold text-xl" style={{ textOverflow: 'hidden' }}>
+                    <span className="font-hairline block text-xs">
                         Previous Post
                     </span>
                     {prev !== null ? prev.frontmatter.title : 'No More Posts'}
                 </div>
             </Link>
 
-            <Link to={next !== null ? next.frontmatter.path : ''} className="paginate bg-primary-grad text-black text-left">
-                <div className={style.copy}>
-                    <span className={style.direction}>
+            <Link to={next !== null ? next.frontmatter.path : ''} className="paginate primary-grad text-black text-left">
+                <div className="truncate relative font-bold text-xl">
+                    <span className="font-hairline block text-xs">
                         Next Post
                     </span>
                     {next !== null ? next.frontmatter.title : 'No More Posts'}
