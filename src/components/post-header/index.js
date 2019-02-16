273f0b460c5390d3512958ad6e02e1d5e29613f7
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-16 10:12:52 -0500

    Replace css with tailwind

--- src/components/post-header/index.js
+++ src/components/post-header/index.js
@@ -3,16 +3,14 @@ import PropTypes from 'prop-types';
 import PostTitle from "../post-title";
 import CategoryImage from "../category-image";
 
-import style from './post-header.module.css';
-
 
 export default function PostHeader({ content }) {
     return (
-        <header className={style.header}>
-            <div className={style.left}>
-                <CategoryImage height={300} width={300} category={content.category} cssClass={style.image} />
+        <header className="header flex flex-col md:flex-row mt-0 mx-auto mb-10 pt-6 pr-4 pb-10 pl-4">
+            <div className="w-full md:w-1/2 self-end mb-4 text-center">
+                <CategoryImage height={300} width={300} category={content.category} cssClass="header-image" />
             </div>
-            <div className={style.right}>
+            <div className="w-full md:w-1/2 self-center">
                 <PostTitle
                     category={content.category}
                     title={content.title}
@@ -22,12 +20,13 @@ export default function PostHeader({ content }) {
                 />
 
                 {content.tags !== null ? (
-                    <div className={style.tags}>
-                        <ul itemProp="keywords">
+                    <div className="flex mt-4">
+                        <ul itemProp="keywords" className="flex flex-wrap list-reset md:justify-between">
                             <li>Tags:&nbsp;</li>
+
                             {content.tags && content.tags.map((tag, i) => {
                                 return (
-                                    <li className={style.tag} key={i}>{tag}</li>
+                                    <li className="bg-white text-xs py-0 px-4 capitalize border-1 border-grey-light mr-2 self-center" key={i}>{tag}</li>
                                 );
                             })}
                         </ul>
