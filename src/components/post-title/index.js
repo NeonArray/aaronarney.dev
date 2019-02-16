4ff518ee2651605837ef7923c64ed7437220c77d
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-16 10:12:40 -0500

    Replace css with tailwind

--- src/components/post-title/index.js
+++ src/components/post-title/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import { assign } from '../../utils';
 import moment from 'moment';
 
-import style from './post-title.module.css';
-import tag from '../../global/tags.module.css';
 
 function createHeader(isH1, headerContent) {
 
     if (isH1) {
         return (
-            <h1 className={style.title} itemProp="headline">
+            <h1 className="text-black font-bold m-0 relative z-50 text-2xl md:text-4xl" itemProp="headline">
                 {headerContent}
             </h1>
         );
     }
 
     return (
-        <h2 className={style.title} itemProp="headline">
+        <h2 className="text-black font-bold m-0 relative z-50 text-2xl md:text-4xl" itemProp="headline">
             {headerContent}
         </h2>
     );
@@ -26,7 +23,7 @@ function createHeader(isH1, headerContent) {
 
 export default function PostTitle({ category, title, date, path, isLink, isH1 = true }) {
     const headerContent = isLink ? (
-        <Link className={style.link} to={path || ''} >
+        <Link className="text-black no-underline" to={path || ''} >
             {title}
         </Link >
     ) : (title);
@@ -34,14 +31,14 @@ export default function PostTitle({ category, title, date, path, isLink, isH1 = 
     return (
         <>
             <div>
-                <Link className={assign(tag[category.toLowerCase()], tag.cat)} to={`blog/category/${category}`}>
+                <Link className={`${category.toLowerCase()} label tag-cat`} to={`blog/category/${category}`}>
                     {category}
                 </Link>
             </div>
 
             {createHeader(isH1, headerContent)}
 
-            <time className={style.date} itemProp="datePublished" dateTime={moment(date).toISOString()}>
+            <time className="pt-2 mt-4 max-w-xs text-base text-grey-darker border-t-1 border-grey-light" itemProp="datePublished" dateTime={moment(date).toISOString()}>
                 {moment(date).format('MMMM DD, YYYY')}
             </time>
         </>
