715a1b31e5551da8cdaedc4569f6fed7453cbbe0
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-16 10:11:25 -0500

    Reprioritize postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
 
 module.exports = () => ({
     plugins: [
-        require('tailwindcss')('./tailwind.js'),
         require('postcss-import'),
+        require('tailwindcss')('./tailwind.js'),
         require('postcss-custom-media'),
         require('postcss-preset-env')({ stage: 0 }),
         require('postcss-nested'),
-        // require('postcss-autoreset'),
         require('postcss-custom-selectors'),
     ],
 });
