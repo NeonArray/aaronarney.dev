afbb93bbdecc97ba225ff945ec97655d2fa208fb
Aaron Arney <aaron.arney+github@ocular-rhythm.com>
2019-02-16 10:11:46 -0500

    Remove unuesd tailwind properties

--- tailwind.js
+++ tailwind.js
@@ -1,142 +1,15 @@
-/*
-
-Tailwind - The Utility-First CSS Framework
-
-A project by Adam Wathan (@adamwathan), Jonathan Reinink (@reinink),
-David Hemphill (@davidhemphill) and Steve Schoger (@steveschoger).
-
-Welcome to the Tailwind config file. This is where you can customize
-Tailwind specifically for your project. Don't be intimidated by the
-length of this file. It's really just a big JavaScript object and
-we've done our very best to explain each section.
-
-View the full documentation at https://tailwindcss.com.
-
-
-|-------------------------------------------------------------------------------
-| The default config
-|-------------------------------------------------------------------------------
-|
-| This variable contains the default Tailwind config. You don't have
-| to use it, but it can sometimes be helpful to have available. For
-| example, you may choose to merge your custom configuration
-| values with some of the Tailwind defaults.
-|
-*/
-
-let defaultConfig = require('tailwindcss/defaultConfig')()
-
-
-/*
-|-------------------------------------------------------------------------------
-| Colors                                    https://tailwindcss.com/docs/colors
-|-------------------------------------------------------------------------------
-|
-| Here you can specify the colors used in your project. To get you started,
-| we've provided a generous palette of great looking colors that are perfect
-| for prototyping, but don't hesitate to change them for your project. You
-| own these colors, nothing will break if you change everything about them.
-|
-| We've used literal color names ("red", "blue", etc.) for the default
-| palette, but if you'd rather use functional names like "primary" and
-| "secondary", or even a numeric scale like "100" and "200", go for it.
-|
-*/
+let defaultConfig = require('tailwindcss/defaultConfig')();
 
 let colors = {
     'transparent': 'transparent',
-
-    'black': '#22292f',
-    'grey-darkest': '#3d4852',
+    'black': '#000',
+    'black-lighter': '#272822',
     'grey-darker': '#606f7b',
-    'grey-dark': '#8795a1',
-    'grey': '#b8c2cc',
     'grey-light': '#dae1e7',
     'grey-lighter': '#f1f5f8',
-    'grey-lightest': '#f8fafc',
     'white': '#ffffff',
-
-    'red-darkest': '#3b0d0c',
-    'red-darker': '#621b18',
-    'red-dark': '#cc1f1a',
-    'red': '#e3342f',
-    'red-light': '#ef5753',
-    'red-lighter': '#f9acaa',
-    'red-lightest': '#fcebea',
-
-    'orange-darkest': '#462a16',
-    'orange-darker': '#613b1f',
-    'orange-dark': '#de751f',
-    'orange': '#f6993f',
-    'orange-light': '#faad63',
-    'orange-lighter': '#fcd9b6',
-    'orange-lightest': '#fff5eb',
-
-    'yellow-darkest': '#453411',
-    'yellow-darker': '#684f1d',
-    'yellow-dark': '#f2d024',
-    'yellow': '#ffed4a',
-    'yellow-light': '#fff382',
-    'yellow-lighter': '#fff9c2',
-    'yellow-lightest': '#fcfbeb',
-
-    'green-darkest': '#0f2f21',
-    'green-darker': '#1a4731',
-    'green-dark': '#1f9d55',
-    'green': '#38c172',
-    'green-light': '#51d88a',
-    'green-lighter': '#a2f5bf',
-    'green-lightest': '#e3fcec',
-
-    'teal-darkest': '#0d3331',
-    'teal-darker': '#20504f',
-    'teal-dark': '#38a89d',
-    'teal': '#4dc0b5',
-    'teal-light': '#64d5ca',
-    'teal-lighter': '#a0f0ed',
-    'teal-lightest': '#e8fffe',
-
-    'blue-darkest': '#12283a',
-    'blue-darker': '#1c3d5a',
-    'blue-dark': '#2779bd',
-    'blue': '#3490dc',
-    'blue-light': '#6cb2eb',
-    'blue-lighter': '#bcdefa',
-    'blue-lightest': '#eff8ff',
-
-    'indigo-darkest': '#191e38',
-    'indigo-darker': '#2f365f',
-    'indigo-dark': '#5661b3',
-    'indigo': '#6574cd',
-    'indigo-light': '#7886d7',
-    'indigo-lighter': '#b2b7ff',
-    'indigo-lightest': '#e6e8ff',
-
-    'purple-darkest': '#21183c',
-    'purple-darker': '#382b5f',
-    'purple-dark': '#794acf',
-    'purple': '#9561e2',
-    'purple-light': '#a779e9',
-    'purple-lighter': '#d6bbfc',
-    'purple-lightest': '#f3ebff',
-
-    'pink-darkest': '#451225',
-    'pink-darker': '#6f213f',
-    'pink-dark': '#eb5286',
-    'pink': '#f66d9b',
-    'pink-light': '#fa7ea8',
-    'pink-lighter': '#ffbbca',
-    'pink-lightest': '#ffebef',
-
-    'black-lighter': '#272822',
-    'or-pink': '#db00a7',
-    'or-teal': '#6CC9D2',
-    'or-red': '#D95A44',
-    'primary-grad': 'radial-gradient(circle farthest-corner at 10% 20%, rgb(147, 207, 216) 0%, var(--primary) 90.1%)',
-    'secondary-grad': 'radial-gradient(circle farthest-corner at 10% 20%, rgb(246, 134, 132) 0%, var(--secondary) 90.1%)',
-    'yellow-grad': 'radial-gradient(circle farthest-corner at 10% 20%, rgb(246, 232, 98) 0%, var(--yellow) 90.1%)',
-    'purple-grad': 'radial-gradient(circle farthest-corner at 10% 20%, rgb(167, 108, 246) 0%, var(--purple) 90.1%)',
-    'blue-grad': 'radial-gradient(circle farthest-corner at 10% 20%, rgb(88, 135, 219) 0%, var(--blue) 90.1%)',
+    'pink': '#db00a7',
+    'red': '#D95A44',
 };
 
 module.exports = {
@@ -263,12 +136,9 @@ module.exports = {
 
     textSizes: {
         'xs': '.75rem',     // 12px
-        'sm': '.875rem',    // 14px
         'base': '1rem',     // 16px
-        'lg': '1.125rem',   // 18px
         'xl': '1.25rem',    // 20px
         '2xl': '1.5rem',    // 24px
-        '3xl': '1.875rem',  // 30px
         '4xl': '2.25rem',   // 36px
         '5xl': '3rem',      // 48px
     },
@@ -291,14 +161,9 @@ module.exports = {
 
     fontWeights: {
         'hairline': 100,
-        'thin': 200,
         'light': 300,
         'normal': 400,
-        'medium': 500,
-        'semibold': 600,
         'bold': 700,
-        'extrabold': 800,
-        'black': 900,
     },
 
 
@@ -318,7 +183,6 @@ module.exports = {
     leading: {
         'none': 1,
         'tight': 1.25,
-        'normal': 1.5,
         'loose': 2,
         'flappy': '76px',
     },
@@ -337,11 +201,7 @@ module.exports = {
     |
     */
 
-    tracking: {
-        'tight': '-0.05em',
-        'normal': '0',
-        'wide': '0.05em',
-    },
+    tracking: {},
 
 
     /*
@@ -392,11 +252,7 @@ module.exports = {
     |
     */
 
-    backgroundSize: {
-        'auto': 'auto',
-        'cover': 'cover',
-        'contain': 'contain',
-    },
+    backgroundSize: {},
 
 
     /*
@@ -418,7 +274,6 @@ module.exports = {
         '0': '0',
         '2': '2px',
         '4': '4px',
-        '8': '8px',
     },
 
 
@@ -460,13 +315,7 @@ module.exports = {
     |
     */
 
-    borderRadius: {
-        'none': '0',
-        'sm': '.125rem',
-        default: '.25rem',
-        'lg': '.5rem',
-        'full': '9999px',
-    },
+    borderRadius: {},
 
 
     /*
@@ -492,32 +341,10 @@ module.exports = {
 
     width: {
         'auto': 'auto',
-        'px': '1px',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-        '24': '6rem',
-        '32': '8rem',
-        '48': '12rem',
-        '64': '16rem',
         '1/2': '50%',
         '1/3': '33.33333%',
-        '2/3': '66.66667%',
         '1/4': '25%',
         '3/4': '75%',
-        '1/5': '20%',
-        '2/5': '40%',
-        '3/5': '60%',
-        '4/5': '80%',
-        '1/6': '16.66667%',
-        '5/6': '83.33333%',
         'full': '100%',
         'screen': '100vw',
     },
@@ -540,23 +367,7 @@ module.exports = {
     */
 
     height: {
-        'auto': 'auto',
-        'px': '1px',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
         '12': '3rem',
-        '16': '4rem',
-        '24': '6rem',
-        '32': '8rem',
-        '48': '12rem',
-        '64': '16rem',
-        'full': '100%',
         'screen': '100vh',
     },
 
@@ -622,14 +433,7 @@ module.exports = {
 
     maxWidth: {
         'xs': '20rem',
-        'sm': '30rem',
-        'md': '40rem',
-        'lg': '50rem',
         'xl': '60rem',
-        '2xl': '70rem',
-        '3xl': '80rem',
-        '4xl': '90rem',
-        '5xl': '100rem',
         'full': '100%',
         'max': '1025px',
         'none': 'none',
@@ -782,10 +586,6 @@ module.exports = {
 
     shadows: {
         default: '0 2px 4px 0 rgba(0,0,0,0.10)',
-        'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
-        'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
-        'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
-        'outline': '0 0 0 3px rgba(52,144,220,0.5)',
         'none': 'none',
         'callout': '8px 8px 0 #efefef',
     },
@@ -809,9 +609,6 @@ module.exports = {
         'auto': 'auto',
         '0': 0,
         '10': 10,
-        '20': 20,
-        '30': 30,
-        '40': 40,
         '50': 50,
     },
 
@@ -830,13 +627,7 @@ module.exports = {
     |
     */
 
-    opacity: {
-        '0': '0',
-        '25': '.25',
-        '50': '.5',
-        '75': '.75',
-        '100': '1',
-    },
+    opacity: {},
 
 
     /*
@@ -901,20 +692,20 @@ module.exports = {
 
     modules: {
         appearance: ['responsive'],
-        backgroundAttachment: ['responsive'],
+        backgroundAttachment: false,
         backgroundColors: ['responsive', 'hover', 'focus'],
-        backgroundPosition: ['responsive'],
-        backgroundRepeat: ['responsive'],
-        backgroundSize: ['responsive'],
-        borderCollapse: [],
+        backgroundPosition: false,
+        backgroundRepeat: false,
+        backgroundSize: false,
+        borderCollapse: false,
         borderColors: ['responsive', 'hover', 'focus'],
-        borderRadius: ['responsive'],
+        borderRadius: false,
         borderStyle: ['responsive'],
         borderWidths: ['responsive'],
         cursor: ['responsive'],
         display: ['responsive'],
         flexbox: ['responsive'],
-        float: ['responsive'],
+        float: false,
         fonts: ['responsive'],
         fontWeights: ['responsive', 'hover', 'focus'],
         height: ['responsive'],
@@ -928,23 +719,23 @@ module.exports = {
         negativeMargin: ['responsive'],
         objectFit: false,
         objectPosition: false,
-        opacity: ['responsive'],
+        opacity: false,
         outline: ['focus'],
         overflow: ['responsive'],
         padding: ['responsive'],
-        pointerEvents: ['responsive'],
+        pointerEvents: false,
         position: ['responsive'],
-        resize: ['responsive'],
+        resize: false,
         shadows: ['responsive', 'hover', 'focus'],
         svgFill: [],
         svgStroke: [],
-        tableLayout: ['responsive'],
+        tableLayout: false,
         textAlign: ['responsive'],
         textColors: ['responsive', 'hover', 'focus'],
         textSizes: ['responsive'],
         textStyle: ['responsive', 'hover', 'focus'],
         tracking: ['responsive'],
-        userSelect: ['responsive'],
+        userSelect: false,
         verticalAlign: ['responsive'],
         visibility: ['responsive'],
         whitespace: ['responsive'],
