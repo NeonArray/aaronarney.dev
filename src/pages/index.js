642863649ca24049f796275aa411635aa06c7cdf
Aaron <ocularrhythm@users.noreply.github.com>
2019-06-16 16:17:32 -0400

    Remove name and company

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,8 @@ export default function Index() {
                     class: 'page-post'
                 }} />
                 <div style={{ textAlign: 'center', maxWidth: '920px', margin: '0 auto', padding: '0 0.5em' }}>
-                    <h1 style={{ lineHeight: '1.25em', fontSize: '2rem' }}>This is a blog about development by and of Aaron Arney, a developer from Louisville, KY.</h1>
-
-                    <p>I am currently a Front-End Developer at <a className="link" href="https://leapsparkagency.com" target="_blank" rel="noopener nofollow noreferrer">Leap Spark Agency</a>, a digital marketing company.</p>
-
+                    <h1 style={{ lineHeight: '1.25em', fontSize: '2rem' }}>This is a blog about development by and of Aaron, a developer from Louisville, KY.</h1>
+        
                     <p>I built this using <a className="link" href="https://www.gatsbyjs.org" target="_blank" rel="noopener nofollow noreferrer">Gatsby 2.0</a> like every other developer. You can also take a look at the <a className="link" href="https://github.com/ocularrhythm/ocular-rhythm-site" target="_blank" rel="noopener nofollow noreferrer">source code</a> if you like.</p>
                 </div>
             </main>
