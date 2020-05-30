75d1366b4f3a65fa4c8692d07d4e5386262520c7
ocularrhythm <github@otacon.press>
2020-05-29 22:31:37 -0400

    Convert Header to TypeScript

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,40 +1,74 @@
-import * as React from 'react';
-import {ReactComponentElement} from "react";
-import { Link } from 'gatsby';
+import * as React from "react";
+import { ReactComponentElement } from "react";
+import { Link } from "gatsby";
 // @ts-ignore
-import * as logo from '../../images/ocular-rhythm-logo.svg';
-
+import * as logo from "../../images/ocular-rhythm-logo.svg";
 
 export default function Header(): ReactComponentElement<any> {
     return (
-        <header data-grid-area="header" className="block w-screen border-2 border-grey-light">
+        <header
+            data-grid-area="header"
+            className="block w-screen border-2 border-grey-light"
+        >
             <div className="my-0 mx-auto max-w-max py-4 px-3 md:px-5">
-
-                <ul className="list-reset flex flex-row items-center" role="navigation">
+                <ul
+                    className="list-reset flex flex-row items-center"
+                    role="navigation"
+                >
                     <li className="flex-1">
                         <Link to="/" title="Go home">
-                            <img src={logo} alt="Ocular Rhythm" height="28" width="186" />
+                            <img
+                                src={logo}
+                                alt="Ocular Rhythm"
+                                height="28"
+                                width="186"
+                            />
                         </Link>
                     </li>
 
                     <li className="border-b-2 border-transparent m-r-3 hover:border-b-2 hover:border-red ml-5">
-                        <Link className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy" to="/blog">Blog</Link>
+                        <Link
+                            className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy"
+                            to="/blog"
+                        >
+                            Blog
+                        </Link>
                     </li>
 
                     <li className="border-b-2 border-transparent m-r-3 hover:border-b-2 hover:border-red ml-5">
-                        <Link className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy" to="/guest-posts">Guest Posts</Link>
+                        <Link
+                            className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy"
+                            to="/guest-posts"
+                        >
+                            Guest Posts
+                        </Link>
                     </li>
 
                     <li className="border-b-2 border-transparent m-r-3 hover:border-b-2 hover:border-red ml-5">
-                        <Link className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy" to="/uses">Uses</Link>
+                        <Link
+                            className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy"
+                            to="/uses"
+                        >
+                            Uses
+                        </Link>
                     </li>
 
                     <li className="border-b-2 border-transparent m-r-3 hover:border-b-2 hover:border-red ml-5">
-                        <Link className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy" to="/fun-stuff">Fun Stuff</Link>
+                        <Link
+                            className="focus:text-gray-dark text-gray-dark font-bold no-underline hover:text-red md:leading-flappy"
+                            to="/fun-stuff"
+                        >
+                            Fun Stuff
+                        </Link>
                     </li>
 
                     <li className="ml-5">
-                        <a href="https://github.com/ocularrhythm" target="_blank" rel="noopener noreferrer nofollow" title="Github Profile - opens in new window">
+                        <a
+                            href="https://github.com/ocularrhythm"
+                            target="_blank"
+                            rel="noopener noreferrer nofollow"
+                            title="Github Profile - opens in new window"
+                        >
                             Github
                         </a>
                     </li>
