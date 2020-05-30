fa8f0651c4975fb711c7b470ee97c6328729a2f9
ocularrhythm <github@otacon.press>
2020-05-29 22:33:04 -0400

    Convert guest-posts to TypeScript

---
+++ src/pages/guest-posts.tsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+
+import Archive from "../components/archive/index";
+import Layout from "../components/layout";
+import { ReactComponentElement } from "react";
+
+export default function GuestPosts(): ReactComponentElement<any> {
+    const edges = [
+        {
+            node: {
+                frontmatter: {
+                    title: "Refactoring JavaScript",
+                    date: "2018-10-18",
+                    path:
+                        "https://www.leapsparkagency.com/blog/leap-spark/2018/10/17/refactoring-javascript",
+                    category: "web",
+                },
+            },
+        },
+        {
+            node: {
+                frontmatter: {
+                    title: "Accessibility Compliance",
+                    date: "2018-12-12",
+                    path:
+                        "https://www.leapsparkagency.com/blog/leap-spark/2018/12/12/accessibility-compliance",
+                    category: "web",
+                },
+            },
+        },
+        {
+            node: {
+                frontmatter: {
+                    title: "Creating a Simple Game with Phaser",
+                    date: "2019-02-13",
+                    path:
+                        "https://www.leapsparkagency.com/blog/leap-spark/2019/02/13/creating-a-simple-game-with-phaser",
+                    category: "web",
+                },
+            },
+        },
+        {
+            node: {
+                frontmatter: {
+                    title: "Migrating from SASS to PostCSS",
+                    date: "2019-03-18",
+                    path:
+                        "https://www.leapsparkagency.com/blog/leap-spark/2019/03/18/migrating-from-sass-to-postcss",
+                    category: "web",
+                },
+            },
+        },
+        {
+            node: {
+                frontmatter: {
+                    title: "What is Personalization",
+                    date: "2019-07-30",
+                    path:
+                        "https://www.progress.com/blogs/what-is-personalization",
+                    category: "web",
+                },
+            },
+        },
+        {
+            node: {
+                frontmatter: {
+                    title: "Regular Expressions for Regular People",
+                    date: "2019-02-07",
+                    path:
+                        "https://alligator.io/js/regular-expressions-for-regular-people/",
+                    category: "web",
+                },
+            },
+        },
+    ];
+
+    return (
+        <Layout>
+            <div style={{ marginTop: "75px" }}>
+                <Helmet
+                    title="Guest Posts | Ocular-Rhythm"
+                    bodyAttributes={{
+                        class: "archive",
+                    }}
+                />
+
+                <Archive centered={true} posts={edges} />
+            </div>
+        </Layout>
+    );
+}
