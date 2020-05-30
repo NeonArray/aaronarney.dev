8cb8031fef807c0a78a7eb4ad4103e90eeed2fa6
ocularrhythm <github@otacon.press>
2020-05-30 14:12:15 -0400

    Update Footer content

--- src/components/Page/footer.tsx
+++ src/components/Page/footer.tsx
@@ -6,11 +6,9 @@ export default function PageFooter(): ReactComponentElement<any> {
     return (
         <div
             data-grid-area="page-footer"
-            className="bg-grey-lighter px-2 py-4 text-center"
+            className="bg-dark-blue px-2 py-4 text-center text-white"
         >
-            <Link className="text-black ml-4" to="/sitemap.xml">
-                Sitemap
-            </Link>
+            &copy; {new Date().getFullYear()} Aaron Arney.
         </div>
     );
 }
