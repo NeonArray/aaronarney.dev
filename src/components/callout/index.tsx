1971e7eacf918c05f6556c91f2203ea412c8e875
ocularrhythm <github@otacon.press>
2020-05-29 22:30:50 -0400

    Convert Callout to TypeScript

--- src/components/callout/index.tsx
+++ src/components/callout/index.tsx
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import {ReactComponentElement} from "react";
+import * as React from "react";
+import { ReactComponentElement } from "react";
 
 interface IProps {
-    message: string;
+    readonly message: string;
 }
 
-export default function Callout({ message }: IProps): ReactComponentElement<any> {
+export default function Callout({
+    message,
+}: IProps): ReactComponentElement<any> {
     return (
         <div className="md:max-w-copy post-copy block text-white bg-pink shadow-callout font-bold my-8 mx-auto max-w-max p-5 uppercase">
-            { message }
+            {message}
         </div>
     );
 }
