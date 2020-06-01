1f8b451e6842051f5f4c975d7551df022640628a
ocularrhythm <github@otacon.press>
2020-05-31 22:53:41 -0400

    Add callout module styles

--- src/components/Post/callout.tsx
+++ src/components/Post/callout.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 import { ReactComponentElement } from "react";
+const styles = require("./callout.module.css");
+
+// @ts-ignore
+import * as alert from "../../images/warning.svg";
 
 interface IProps {
     readonly message: string;
@@ -10,6 +14,7 @@ export default function Callout({
 }: IProps): ReactComponentElement<any> {
     return (
         <div className="md:max-w-copy post-copy block text-white bg-pink shadow-callout font-bold my-8 mx-auto max-w-max p-5 uppercase">
+            <img src={alert} className={styles.callout} alt=""/>
             {message}
         </div>
     );
