b29722d47115d696073bc087572f9182af7ff594
Aaron Arney <aarney@leapsparkagency.com>
2020-06-29 16:56:00 -0400

    Include /work directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,13 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/work`,
+                name: `work`,
+            },
+        },
+        {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
