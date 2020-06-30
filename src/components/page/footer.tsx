edb6a611984785895a0bc0299fa7aaa040a80985
Aaron Arney <aarney@leapsparkagency.com>
2020-06-30 15:51:36 -0400

    Add footer component but remove content

--- src/components/page/footer.tsx
+++ src/components/page/footer.tsx
@@ -6,8 +6,6 @@ export default function PageFooter(): ReactComponentElement<any> {
         <div
             data-grid-area="page-footer"
             className="bg-dark-blue px-2 py-4 text-center text-white"
-        >
-            &copy; {new Date().getFullYear()} Aaron Arney.
-        </div>
+        />
     );
 }
