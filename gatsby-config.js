d070ab52c9dc1a9b684413a47421ab969c94551d
Aaron Arney <aarney@leapsparkagency.com>
2020-07-21 12:49:33 -0400

    Implement RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,60 @@ module.exports = {
     plugins: [
         `gatsby-plugin-netlify-cms`,
         `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                  {
+                    site {
+                      siteMetadata {
+                        title
+                        description
+                        siteUrl
+                        site_url: siteUrl
+                      }
+                    }
+                  }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ "content:encoded": edge.node.html }],
+                                })
+                            })
+                        },
+                        query: `
+                        {
+                            allMarkdownRemark(
+                                filter: { frontmatter: { type: { ne: "work" } } }
+                                sort: { order: DESC, fields: [frontmatter___date] },
+                            ) {
+                                edges {
+                                    node {
+                                        excerpt
+                                        html
+                                        fields { slug }
+                                        frontmatter {
+                                            title
+                                            date
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    `,
+                        output: "/rss.xml",
+                        title: "Aaron Arney's RSS Feed",
+                    },
+                ],
+            },
+        },
         `gatsby-transformer-sharp`,
         {
             resolve: `gatsby-source-filesystem`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,20 @@
 const path = require("path");
 const { createFilePath } = require("gatsby-source-filesystem");
 
+exports.onCreateNode = ({ node, actions, getNode }) => {
+    const {createNodeField} = actions;
+
+    if (node.internal.type === "MarkdownRemark") {
+        const value = createFilePath({ node, getNode });
+
+        createNodeField({
+            name: "slug",
+            node,
+            value,
+        });
+    }
+};
+
 exports.createPages = async ({ actions, graphql }) => {
     const { createPage } = actions;
 
